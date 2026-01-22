6efe1dcc16d9fff74a1be9849ff323d641997029
Rakib Hasan Shawon <shawonetc@gmail.com>
2026-01-22 16:21:15 +0600

    Regenerate package-lock.json to fix npm ci error

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
         {children}
-      </body>
+      </body> 
     </html>
   );
 }
